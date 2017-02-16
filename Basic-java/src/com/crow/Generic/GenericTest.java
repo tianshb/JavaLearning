a163370966186a51a0ff420f30d5f289ce1a9e4e
CrowHawk <wwwzeyuan@vip.qq.com>
2017-02-16 16:42:00 +0800

    Generic2

--- Basic-java/src/com/crow/Generic/GenericTest.java
+++ Basic-java/src/com/crow/Generic/GenericTest.java
@@ -4,14 +4,31 @@ package com.crow.Generic;
  * Created by CrowHawk on 17/2/16.
  */
 
+
+import java.lang.reflect.*;
 import java.util.*;
 
 public class GenericTest {
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception{
         HashMap<String, Integer> hashMap = new HashMap<>();
         hashMap.put("Tom", 1);
         hashMap.put("Jerry", 2);
         hashMap.put("Nick", 3);
+        printHashmap(hashMap);
+
+        String[] array = new String[]{"crow","martin","pattie"};
+        swap(array, 1, 2);
+        System.out.println("array[2] = " + array[2]);
+
+        Method printMethod = GenericTest.class.getMethod("printHashmap", HashMap.class);
+        Type[] types = printMethod.getGenericParameterTypes();
+        ParameterizedType parameterizedType = (ParameterizedType)types[0];
+        Type[] actualTypes = parameterizedType.getActualTypeArguments();
+        System.out.println("The paratype is " + parameterizedType.getRawType() + "<" + actualTypes[0] + "," + actualTypes[1] + ">");
+    }
+
+
+    public static void printHashmap(HashMap<String, Integer> hashMap){
         Set<Map.Entry<String, Integer>> entrySet = hashMap.entrySet();
         for(Map.Entry<String, Integer> entry: entrySet){
             System.out.println(entry.getKey() + "," + entry.getValue());
@@ -22,5 +39,11 @@ public class GenericTest {
             System.out.print("Key is " + entry.getKey() + ",");
             System.out.println("Value is " + entry.getValue());
         }
+    }
+
+    public static <T> void swap(T[] arr, int i, int j){
+        T tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
     }
 }
